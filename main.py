b2c7a2c17c0874684eb9bfa65ba072588ea9709d
Nathan Cassereau <nathan.cassereau@idris.fr>
2021-11-09 16:32:15 +0100

    conf

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import OmegaConf
 import hydra
+from hydra.core.config_store import ConfigStore
+from stylebank.dataclasses import Configuration
 
+
+cs = ConfigStore.instance()
+cs.store(name="base_config", node=Configuration)
 
 @hydra.main(config_path="conf", config_name="config")
-def main(cfg: DictConfig) -> None:
+def main(cfg: Configuration) -> None:
     print(OmegaConf.to_yaml(cfg))
 
 
